dd5c983af39ab1ffbfa724111af6a00c37087941
mrLordi <d.vodotiiets@yandex.ua>
2017-04-19 05:50:15 +0300

    fixed

--- ada_semaphore/src/main.adb
+++ ada_semaphore/src/main.adb
@@ -1,7 +1,7 @@
 -----------------------------------------------
 -----   Lab 1. Ada. Semaphore             -----
 -----   A = max(Z) * B + z * (MO * MT)    -----
------   Denys Vodotiiets                -----
+-----   Denys Vodotiiets                  -----
 -----   IP-42                             -----
 -----   22.03.17                          -----
 -----------------------------------------------
@@ -12,16 +12,14 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Synchronous_Task_Control; use Ada.Synchronous_Task_Control;
 
 procedure Main is
-   N: Integer := 2;
+   N: Integer := 6;
    P: Integer := 2;
    H: Integer := N/P;
    s0, s1, s2, s3, s4, s5: Suspension_Object;
    type Vector is array(1..N) of Integer;
    type Matrix is array(1..N) of Vector;
-   z, a : Integer;
-   a1 : Integer := Integer'First;
-   a2 : Integer := Integer'First;
-   Result, B, Z_Vector: Vector;
+   a : Integer := Integer'First;
+   Result, B, Z: Vector;
    MO, MT: Matrix;
 
 
@@ -59,14 +57,14 @@ procedure Main is
       task T1;
       task body T1 is
          MO1: Matrix;
-         z1: Integer;
+         Z1, Temp: Vector;
+         a1 : Integer := Integer'First;
          sum: Integer;
       begin
          Put_Line(">>T1 started");
 
          -- entering data
-         inputVector(Z_Vector);
-         z := 1;
+         inputVector(Z);
 
          -- send signal to T2 about end of input
          Set_True(s1);
@@ -76,29 +74,25 @@ procedure Main is
 
          -- copy mutual resources
          Suspend_Until_True(s0);
-         z1 := z;
-         for i in 1..N loop
-            for j in 1..N loop
-               MO1(i)(j) := MO(i)(j);
-            end loop;
-         end loop;
+         Z1 := Z;
+         MO1 := MO;
          Set_True(s0);
 
          -- calculating
          for i in 1..H loop
-            if Z_Vector(i) > a1 then
-               a1 := Z_Vector(i);
+            if Z1(i) > a1 then
+               a1 := Z1(i);
             end if;
          end loop;
 
+         Suspend_Until_True(s0);
+         if a1 > a then
+            a := a1;
+         end if;
+         Set_True(s0);
+
          -- wait while T2 is calculating
          Suspend_Until_True(s3);
-
-         if a1 > a2 then
-            a := a1;
-         else
-            a := a2;
-         end if;
 
          -- send signal to T2 about end of calculating
          Set_True(s4);
@@ -109,15 +103,21 @@ procedure Main is
          Set_True(s0);
 
          -- calculating
-         for j in 1..H loop
-            for i in 1..N loop
+         for i in 1..H loop
+            for j in 1..N loop
                sum := 0;
-               for k in 1..N loop
-                  sum := sum + MO1(i)(k) * MT(j)(k);
+               for l in 1..N loop
+                  sum := sum + MO1(l)(j) * MT(i)(l);
                end loop;
-               Result(j) := sum * z1;
+               Temp(j) := sum;
             end loop;
-            Result(j) := Result(j) + a1 * B(j);
+
+            sum := 0;
+            for j in 1..N loop
+               sum := sum + Temp(j) * Z1(j);
+            end loop;
+
+            Result(i) := a1 * B(i) + sum;
          end loop;
 
          -- wait while T2 is calculating
@@ -133,7 +133,8 @@ procedure Main is
       task T2;
       task body T2 is
          MO2: Matrix;
-         z2: Integer;
+         Z2, Temp: Vector;
+         a2 : Integer := Integer'First;
          sum: Integer;
       begin
          Put_Line(">>T2 started");
@@ -151,20 +152,22 @@ procedure Main is
 
          -- copy mutual resources
          Suspend_Until_True(s0);
-         z2 := z;
-         for i in 1..N loop
-            for j in 1..N loop
-               MO2(i)(j) := MO(i)(j);
-            end loop;
-         end loop;
+         Z2 := Z;
+         MO2 := MO;
          Set_True(s0);
 
          -- calculating
          for i in H + 1..N loop
-            if Z_Vector(i) > a2 then
-               a2 := Z_Vector(i);
+            if Z2(i) > a2 then
+               a2 := Z2(i);
             end if;
          end loop;
+
+         Suspend_Until_True(s0);
+         if a2 > a then
+            a := a2;
+         end if;
+         Set_True(s0);
 
          -- send signal to T1 about end of calculating
          Set_True(s3);
@@ -174,19 +177,25 @@ procedure Main is
 
           -- copy mutual resources
          Suspend_Until_True(s0);
-         a1 := a;
+         a2 := a;
          Set_True(s1);
 
          -- calculating
-         for j in H+1..N loop
-            for i in 1..N loop
+         for i in H+1..N loop
+            for j in 1..N loop
                sum := 0;
-               for k in 1..N loop
-                  sum := sum + MO2(i)(k) * MT(j)(k);
+               for l in 1..N loop
+                  sum := sum + MO2(l)(j) * MT(i)(l);
                end loop;
-               Result(j) := sum * z2;
+               Temp(j) := sum;
             end loop;
-            Result(j) := Result(j) + a2 * B(j);
+
+            sum := 0;
+            for j in 1..N loop
+               sum := sum + Temp(j) * Z2(j);
+            end loop;
+
+            Result(i) := a2 * B(i) + sum;
          end loop;
 
           -- send signal to T1 about end of calculating
